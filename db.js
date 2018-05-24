c9833ec87577e16dcf6e2846f7af67ee04119229
soju <frankyiyi@gmail.com>
2018-05-23 23:43:28 -0700

    1234

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ const user = process.env.DB_USER
 const pass = process.env.DB_PASS
 
 module.exports = {
-    url:`mongodb://sojuadmin:Neonblu3@ds133360.mlab.com:33360/testing`
+    url:`mongodb://sojuadmin:Neonblu3@ds233970.mlab.com:33970/girls`
 }
