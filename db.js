c329acce196c02b6655242077e61c5f86be576a5
soju <frankyiyi@gmail.com>
2018-05-23 18:30:28 -0700

    updated heroku user env

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
+
 module.exports = {
-    url:`mongodb://sojuadmin:Neonblu3@ds133360.mlab.com:33360/testing`
+    url:`mongodb://${user}:${pass}@ds133360.mlab.com:33360/testing`
 }
