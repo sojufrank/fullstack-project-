2c293da0f741ca0c9da0f76211555924765e5872
soju <frankyiyi@gmail.com>
2018-05-23 17:04:53 -0700

    troubleshooting heroku

--- index.js
+++ index.js
@@ -35,5 +35,6 @@ app.use((err, req, res, next) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
+    var port = server.address().port;
     console.log('App started, now listening for requests')
 })
